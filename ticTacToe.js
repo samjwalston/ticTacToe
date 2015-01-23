8d3bb2083edf4319476dae1d59ab27d66c151c29
samjwalston <samjwalston@gmail.com>
2015-01-23 07:41:22 -0800

    Added Stats Box and various animations

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,8 +1,10 @@
 // Variables
-var counter = 1;	// Move Counter
-var turn = 0;	// Turn variable (0 equals player one and 1 equals player 2)
-var p1Arr = [];
-var p2Arr = [];
+var turn = 1;	// Move Counter
+var player = 1;	// Turn variable (1 equals player one and 2 equals player two)
+var playerChoices = [[],[]];
+var p1Wins = 0;
+var p2Wins = 0;
+var ties = 0;
 
 // End Games Messages
 function endGame(ending){
@@ -26,19 +28,57 @@ function endGame(ending){
 
 // Resets the board
 function reset(){
-	counter = 0;
-	turn = 0;
-	p1Arr = [];
-	p2Arr = [];
+	turn = 1;
+	player = 1;
+	playerChoices = [[],[]];
 	for(z = 1; z <= 9; ++z){
 		resetColor = document.getElementById('sq' + z.toString());
-		resetColor.style.color = "#be9b7b";
+		resetColor.style.color = "#fff4e6";
 	}
 	for(y = 1; y <= 9; ++y){
 		resetValue = document.getElementById('sq' + y.toString());
-		resetValue.value = "   ";
+		resetValue.innerHTML = "";
 	}
   endGame("clear");
+}
+function mouseOver(number){
+	hover = document.getElementById('sq' + number.toString());
+	if((hover.innerHTML !== "X") && (hover.innerHTML !== "O")){
+		if(player == 2){
+			hover.innerHTML = " O ";
+			hover.style.opacity = "0.5";
+			hover.style.color = "#be9b7b";
+		}
+		else if(player == 1){
+			hover.innerHTML = " X ";
+			hover.style.opacity = "0.3";
+			hover.style.color = "#854442";
+		}
+	}
+};
+function mouseOut(number){
+	out = document.getElementById('sq' + number.toString());
+	if((out.innerHTML !== "X") && (out.innerHTML !== "O")){
+		out.innerHTML = "";
+	}
+};
+function statsBoard(){
+	board = document.getElementById('gameStats');
+	totalGames = (p1Wins + p2Wins + ties);
+	printTotalGames = "Total Games: " + totalGames;
+	printP1Wins = "Wins: " + p1Wins + " (" + ((p1Wins / totalGames) * 100).toFixed(1) + "%)";
+	printP1Losses = "Losses: " + p2Wins + " (" + ((p2Wins / totalGames) * 100).toFixed(1) + "%)";
+	printP2Wins = "Wins: " + p2Wins + " (" + ((p2Wins / totalGames) * 100).toFixed(1) + "%)";
+	printP2Losses = "Losses: " + p1Wins + " (" + ((p1Wins / totalGames) * 100).toFixed(1) + "%)";
+	printTies = "Ties: " + ties + " (" + ((ties / totalGames) * 100).toFixed(1) + "%)";
+	document.getElementById('totalGames').innerHTML = printTotalGames;
+	document.getElementById('p1Wins').innerHTML = printP1Wins;
+	document.getElementById('p1Losses').innerHTML = printP1Losses;
+	document.getElementById('p2Wins').innerHTML = printP2Wins;
+	document.getElementById('p2Losses').innerHTML = printP2Losses;
+	document.getElementById('totalTies').innerHTML = printTies;
+	board.style.visibility = "visible";
+	board.className = "animated fadeIn";
 }
 
 // Main function that takes a value depending on which button is clicked and assigns it to
@@ -56,61 +96,67 @@ function userChoice(number){
 		// Checks all the win conditions for Players
 		// Finds if the index number of the given value is greater than or equal to 0. Which
 		// 		means that is in the array. (If the value was not in the array it would return -1).
-		if(((p1Arr.indexOf(1) >= 0) && (p1Arr.indexOf(2) >= 0) && (p1Arr.indexOf(3) >= 0)) ||
-			((p1Arr.indexOf(4) >= 0) && (p1Arr.indexOf(5) >= 0) && (p1Arr.indexOf(6) >= 0)) ||
-			((p1Arr.indexOf(7) >= 0) && (p1Arr.indexOf(8) >= 0) && (p1Arr.indexOf(9) >= 0)) ||
-			((p1Arr.indexOf(2) >= 0) && (p1Arr.indexOf(5) >= 0) && (p1Arr.indexOf(8) >= 0)) ||
-			((p1Arr.indexOf(3) >= 0) && (p1Arr.indexOf(6) >= 0) && (p1Arr.indexOf(9) >= 0)) ||
-			((p1Arr.indexOf(1) >= 0) && (p1Arr.indexOf(4) >= 0) && (p1Arr.indexOf(7) >= 0)) ||
-			((p1Arr.indexOf(1) >= 0) && (p1Arr.indexOf(5) >= 0) && (p1Arr.indexOf(9) >= 0)) ||
-			((p1Arr.indexOf(3) >= 0) && (p1Arr.indexOf(5) >= 0) && (p1Arr.indexOf(7) >= 0))){
-				// If any of the combinations is true changes the turn number to 2 so no more squares
-				// 		can be changed. Then prints the winner message.
-				turn = 2;
-				endGame("p1Won");
-		}
-		else if(((p2Arr.indexOf(1) >= 0) && (p2Arr.indexOf(2) >= 0) && (p2Arr.indexOf(3) >= 0)) ||
-			((p2Arr.indexOf(4) >= 0) && (p2Arr.indexOf(5) >= 0) && (p2Arr.indexOf(6) >= 0)) ||
-			((p2Arr.indexOf(7) >= 0) && (p2Arr.indexOf(8) >= 0) && (p2Arr.indexOf(9) >= 0)) ||
-			((p2Arr.indexOf(2) >= 0) && (p2Arr.indexOf(5) >= 0) && (p2Arr.indexOf(8) >= 0)) ||
-			((p2Arr.indexOf(3) >= 0) && (p2Arr.indexOf(6) >= 0) && (p2Arr.indexOf(9) >= 0)) ||
-			((p2Arr.indexOf(1) >= 0) && (p2Arr.indexOf(4) >= 0) && (p2Arr.indexOf(7) >= 0)) ||
-			((p2Arr.indexOf(1) >= 0) && (p2Arr.indexOf(5) >= 0) && (p2Arr.indexOf(9) >= 0)) ||
-			((p2Arr.indexOf(3) >= 0) && (p2Arr.indexOf(5) >= 0) && (p2Arr.indexOf(7) >= 0))){
-				turn = 2;
-				endGame("p2Won");
-		}
-		// Checks to see if the counter has reached 10 meaning the board is filled up and the game
-		// 		is a tie.
-		else if(counter == 10){
-			endGame("tied");
+		for(a = 0; a < 2; ++a){
+			console.log(playerChoices[a]);
+			if(((playerChoices[a].indexOf(1) >= 0) && (playerChoices[a].indexOf(2) >= 0) && (playerChoices[a].indexOf(3) >= 0)) ||
+				((playerChoices[a].indexOf(4) >= 0) && (playerChoices[a].indexOf(5) >= 0) && (playerChoices[a].indexOf(6) >= 0)) ||
+				((playerChoices[a].indexOf(7) >= 0) && (playerChoices[a].indexOf(8) >= 0) && (playerChoices[a].indexOf(9) >= 0)) ||
+				((playerChoices[a].indexOf(2) >= 0) && (playerChoices[a].indexOf(5) >= 0) && (playerChoices[a].indexOf(8) >= 0)) ||
+				((playerChoices[a].indexOf(3) >= 0) && (playerChoices[a].indexOf(6) >= 0) && (playerChoices[a].indexOf(9) >= 0)) ||
+				((playerChoices[a].indexOf(1) >= 0) && (playerChoices[a].indexOf(4) >= 0) && (playerChoices[a].indexOf(7) >= 0)) ||
+				((playerChoices[a].indexOf(1) >= 0) && (playerChoices[a].indexOf(5) >= 0) && (playerChoices[a].indexOf(9) >= 0)) ||
+				((playerChoices[a].indexOf(3) >= 0) && (playerChoices[a].indexOf(5) >= 0) && (playerChoices[a].indexOf(7) >= 0))){
+					// If any of the combinations are true changes the player to 0 so no more squares
+					// 		can be changed. Then prints the winner message.
+					player = 0;
+					if(a == 0){
+						endGame("p1Won");
+						p1Wins++;
+						statsBoard();
+					}
+					else if(a == 1){
+						endGame("p2Won");
+						p2Wins++;
+						statsBoard();
+					}
+			}
+			// Checks to see if the counter has reached 10 meaning the board is filled up and the game
+			// 		is a tie.
+			else if(turn == 10){
+				endGame("tied");
+				ties++;
+				statsBoard();
+			}
 		}
 	}
 
 	// Checks whether the turn is equal to 0 (Player 1)
-	if(turn == 0){
+	if(player == 1){
 		// Checks the see which value the 'number' variable is and that that square is blank
 		for(i = 1; i <= 9; ++i){
-			if((number == i) && (square.value == "   ")){
+			if((number == i) && (square.innerHTML !== "X") && (square.innerHTML !== "O")){
 				// If those conditions are true then it sets the value of that square to the desired
 				// 		value and styling. Then pushes that 'number' into that players array. Adds one to
-				// 		the move 'counter' variable. Then switches the 'turn' variable to 1 (Player 2).
-				square.value = " X ";
+				// 		the 'turn' counter variable. Then switches the 'turn' variable to 1 (Player 2).
+				square.innerHTML = "X";
+				square.style.opacity = "1";
 				square.style.color = "#854442";
-				p1Arr.push(number);
-				counter++;
-				turn = 1;
+				playerChoices[0].push(number);
+				turn++;
+				player = 2;
 			}
 		}
 		winConditions();
 	}
-	else if(turn == 1){
+	else if(player == 2){
 		for(x = 1; x <= 9; ++x){
-			if((number == x) && (square.value == "   ")){
-				square.value = " O ";
-				p2Arr.push(number);
-				counter++;
-				turn = 0;
+			if((number == x) && (square.innerHTML !== "X") && (square.innerHTML !== "O")){
+				square.innerHTML = "O";
+				square.style.opacity = "1";
+				square.style.color = "#be9b7b";
+				playerChoices[1].push(number);
+				turn++;
+				player = 1;
 			}
 		}
 		winConditions();
